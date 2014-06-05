0f73fc663a1f943603e780d9413d43c56f12f8e7
Pavel Yushchenko <pavelyushchenko@gmail.com>
2014-06-05 18:21:47 +0400

    new staff

--- lms/djangoapps/dashboard/staff_urls.py
+++ lms/djangoapps/dashboard/staff_urls.py
@@ -11,4 +11,6 @@ urlpatterns = patterns(
     '',
     url(r'^$', staff.ImportCert.as_view(), name="staff"),
     url(r'^cert/?$', staff.ImportCert.as_view(), name="staff_cert"),
+    url(r'^stat/?$', staff.Stat.as_view(), name="staff_stat"),
+    url(r'^settings/?$', staff.StaffSettings.as_view(), name="staff_settings"),
 )
