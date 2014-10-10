7fd0382c71f621567a240e0478343c9ab346b600
Eugene Medvedev <rn3aoh.g@gmail.com>
2014-10-10 15:14:33 +0400

    Inline UserProfile in admin for User

--- common/djangoapps/student/admin.py
+++ common/djangoapps/student/admin.py
@@ -6,6 +6,30 @@ from student.models import UserProfile, UserTestGroup, CourseEnrollmentAllowed
 from student.models import CourseEnrollment, Registration, PendingNameChange
 from ratelimitbackend import admin
 
+# Mihara: Adding inline UserProfile to user.
+
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+
+class UserWithProfileAdmin(UserAdmin):
+    inlines = [UserProfileInline, ]
+
+    list_display = (
+        'id',
+        'username',
+        'email',
+        'is_superuser',
+        'is_staff',
+        'is_active',
+    )
+
+
+#admin.site.unregister(User)
+admin.site.register(User, UserWithProfileAdmin)
+
 admin.site.register(UserProfile)
 
 admin.site.register(UserTestGroup)
@@ -17,3 +41,4 @@ admin.site.register(CourseEnrollmentAllowed)
 admin.site.register(Registration)
 
 admin.site.register(PendingNameChange)
+
