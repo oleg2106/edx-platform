81c91db22e492f71e5e89fbdb6de4afe8a8e95df
Eugene Medvedev <rn3aoh.g@gmail.com>
2014-10-20 21:27:27 +0400

    We settled on that version for now...

--- lms/lib/comment_client/user.py
+++ lms/lib/comment_client/user.py
@@ -29,7 +29,7 @@ class User(models.Model):
         # make usernames invisible -- because a LOT of our usernames are email addresses!
         return cls(id=str(user.id),
                    external_id=str(user.id),
-                   username=u"{0} {1}.".format((user.last_name or ''),(user.first_name or '')[:1]))
+                   username=u"[{0}] {1}".format(hex(user.id)[2:].zfill(6),user.profile.name))
 
     def follow(self, source):
         params = {'source_type': source.type, 'source_id': source.id}
