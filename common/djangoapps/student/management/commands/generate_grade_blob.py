91ee9a378318580693397f5223101ebd92fc5bce
Eugene Medvedev <rn3aoh.g@gmail.com>
2016-03-30 18:36:29 +0300

    Adapt dumper for Dogwood
    Also adds new command line options and removes role dump.

--- common/djangoapps/student/management/commands/generate_grade_blob.py
+++ common/djangoapps/student/management/commands/generate_grade_blob.py
@@ -19,17 +19,9 @@ from django.contrib.auth.models import User
 
 from xmodule.modulestore.django import modulestore
 from xmodule.modulestore.exceptions import ItemNotFoundError
-from student.models import CourseEnrollment, CourseAccessRole, anonymous_id_for_user
+from student.models import CourseEnrollment, anonymous_id_for_user
 from courseware.grades import iterate_grades_for
-from courseware.courses import course_image_url
-from django_comment_common.models import Role, FORUM_ROLE_ADMINISTRATOR, \
-                                         FORUM_ROLE_MODERATOR, FORUM_ROLE_COMMUNITY_TA
-
-IMPORTANT_ROLES = {
-    "administrator": FORUM_ROLE_ADMINISTRATOR,
-    "moderator": FORUM_ROLE_MODERATOR,
-    "assistant": FORUM_ROLE_COMMUNITY_TA,
-    }
+from openedx.core.lib.courses import course_image_url
 
 class Command(BaseCommand):
     can_import_settings = True
@@ -48,6 +40,16 @@ class Command(BaseCommand):
                     dest='exclude_file',
                     default=False,
                     help='Name of the list of excluded courses. Optional'),
+        make_option('-i', '--include',
+                    metavar='INCLUDE_FILE',
+                    dest='include_file',
+                    default=False,
+                    help='Name of the list of included courses. Optional'),
+        make_option('-c', '--course',
+                    metavar='SINGLE_COURSE',
+                    dest='single_course',
+                    default=False,
+                    help='Name of a single course to dump. Optional'),
         make_option('-o', '--output',
                     metavar='FILE',
                     dest='output',
@@ -70,6 +72,7 @@ class Command(BaseCommand):
             return thing
 
         exclusion_list = []
+        inclusion_list = []
 
         if options['exclude_file']:
             try:
@@ -78,6 +81,14 @@ class Command(BaseCommand):
                 exclusion_list = [x.strip() for x in data]
             except IOError:
                 raise CommandError("Could not read exclusion list from '{0}'".format(options['exclude_file']))
+
+        if options['include_file']:
+            try:
+                with open(options['include_file'],'rb') as inclusion_file:
+                    data = inclusion_file.readlines()
+                inclusion_list = [x.strip() for x in data]
+            except IOError:
+                raise CommandError("Could not read inclusion list from '{0}'".format(options['include_file']))
 
         store = modulestore()
         epoch = int(time.time())
@@ -90,83 +101,75 @@ class Command(BaseCommand):
 
             course_id_string = course.id.to_deprecated_string()
 
-            if course_id_string in exclusion_list:
-                print "Skipping {0} by exclusion list.".format(course_id_string)
-                continue
-            else:
-                print "Processing {0}".format(course_id_string)
-                forum_roles = {}
-                for packet_name, role_name in IMPORTANT_ROLES.iteritems():
-                    try:
-                        forum_roles[packet_name] = [
-                            x.username for x in
-                            Role.objects.get(course_id=course.id, name=role_name).users.all()
-                        ]
-                    except Role.DoesNotExist:
-                        pass
+            if options['single_course']:
+                if course_id_string not in [options['single_course'].strip()]:
+                    continue
+            elif inclusion_list:
+                if not course_id_string in inclusion_list:
+                    continue
+            elif exclusion_list:
+                if course_id_string in exclusion_list:
+                    continue
 
-                students = CourseEnrollment.objects.users_enrolled_in(course.id)
+            print "Processing {}".format(course_id_string)
 
-                course_block = {
-                  'id': course_id_string,
-                  'meta_data': {
-                    'about': {
-                        'display_name': course.display_name,
-                        'media': {
-                            'course_image': course_image_url(course),
-                        }
-                    },
-                    # Yes, I'm duplicating them for now, because the about section is shot.
+            students = CourseEnrollment.objects.users_enrolled_in(course.id)
+
+            course_block = {
+              'id': course_id_string,
+              'meta_data': {
+                'about': {
                     'display_name': course.display_name,
-                    'banner': course_image_url(course),
-                    'id_org': course.org,
-                    'id_number': course.number,
-                    'graded': course.graded,
-                    'hidden': course.visible_to_staff_only,
-                    'ispublic': course.ispublic,
-                    'lowest_passing_grade': course.lowest_passing_grade,
-                    'start': iso_date(course.start),
-                    'advertised_start': iso_date(course.advertised_start),
-                    'end': iso_date(course.end),
-                    'enrollment_end': iso_date(course.enrollment_end),
-                    'enrollment_start': iso_date(course.enrollment_start),
-                    'has_started': course.has_started(),
-                    'has_ended': course.has_ended(),
-                    'overview': get_detail(course.id,'overview'),
-                    'short_description': get_detail(course.id,'short_description'),
-                    'pre_requisite_courses': get_detail(course.id,'pre_requisite_courses'),
-                    'video': get_detail(course.id,'video'),
-                  },
-                  'staff_data': {
-                    'instructors': [x.user.username for x in CourseAccessRole.objects.filter(course_id=course.id, role='instructor')],
-                    'staff': [x.user.username for x in CourseAccessRole.objects.filter(course_id=course.id, role='staff')],
-                    'forum': forum_roles,
-                  },
-                  'students': [x.username for x in students],
-                  'global_anonymous_id': { x.username:anonymous_id_for_user(x, None) for x in students },
-                  'local_anonymous_id': { x.username:anonymous_id_for_user(x, course.id) for x in students },
-                }
+                    'media': {
+                        'course_image': course_image_url(course),
+                    }
+                },
+                # Yes, I'm duplicating them for now, because the about section is shot.
+                'display_name': course.display_name,
+                'banner': course_image_url(course),
+                'id_org': course.org,
+                'id_number': course.number,
+                'graded': course.graded,
+                'hidden': course.visible_to_staff_only,
+                'ispublic': course.ispublic,
+                'lowest_passing_grade': course.lowest_passing_grade,
+                'start': iso_date(course.start),
+                'advertised_start': iso_date(course.advertised_start),
+                'end': iso_date(course.end),
+                'enrollment_end': iso_date(course.enrollment_end),
+                'enrollment_start': iso_date(course.enrollment_start),
+                'has_started': course.has_started(),
+                'has_ended': course.has_ended(),
+                'overview': get_detail(course.id,'overview'),
+                'short_description': get_detail(course.id,'short_description'),
+                'pre_requisite_courses': get_detail(course.id,'pre_requisite_courses'),
+                'video': get_detail(course.id,'video'),
+              },
+              'students': [x.username for x in students],
+              'global_anonymous_id': { x.username:anonymous_id_for_user(x, None) for x in students },
+              'local_anonymous_id': { x.username:anonymous_id_for_user(x, course.id) for x in students },
+            }
 
-                if not options['meta_only']:
-                    blob['grading_data_epoch'] = epoch
-                    course_block['grading_data'] = []
-                    # Grab grades for all students that have ever had anything to do with the course.
-                    graded_students = User.objects.filter(pk__in=CourseEnrollment.objects.filter(course_id=course.id).values_list('user',flat=True))
-                    print "{0} graded students in course {1}".format(graded_students.count(),course_id_string)
-                    if graded_students.count():
-                        for student, gradeset, error_message \
-                            in iterate_grades_for(course.id, graded_students):
-                            if gradeset:
-                                course_block['grading_data'].append({
-                                    'username': student.username,
-                                    'grades': gradeset,
-                                })
-                            else:
-                                print error_message
+            if not options['meta_only']:
+                blob['grading_data_epoch'] = epoch
+                course_block['grading_data'] = []
+                # Grab grades for all students that have ever had anything to do with the course.
+                graded_students = User.objects.filter(pk__in=CourseEnrollment.objects.filter(course_id=course.id).values_list('user',flat=True))
+                print "{0} graded students in course {1}".format(graded_students.count(),course_id_string)
+                if graded_students.count():
+                    for student, gradeset, error_message \
+                        in iterate_grades_for(course.id, graded_students):
+                        if gradeset:
+                            course_block['grading_data'].append({
+                                'username': student.username,
+                                'grades': gradeset,
+                            })
+                        else:
+                            print error_message
 
-                blob['courses'].append(course_block)
+            blob['courses'].append(course_block)
         if options['output']:
-            # Ensure the dump is atomic. 
+            # Ensure the dump is atomic.
             with tempfile.NamedTemporaryFile('w', dir=os.path.dirname(options['output']), delete=False) as output_file:
                 json.dump(blob, output_file)
                 tempname = output_file.name
