6a01607b813f46a599a6a875741197dd913692eb
Eugene Medvedev <rn3aoh.g@gmail.com>
2017-12-13 12:52:02 +0300

    Blob dumper fully restored, functionality abstracted out.

--- common/djangoapps/kursitet/management/commands/generate_grade_blob.py
+++ common/djangoapps/kursitet/management/commands/generate_grade_blob.py
@@ -3,26 +3,18 @@ Management command that will generate a blob of all grades and course metadata
 as a convenient json file which is easy to subsequently serve
 to external systems.
 """
+
 import time
-import calendar
-import datetime
 import json
 import tempfile
 import os
 from optparse import make_option
 
 from django.core.management.base import BaseCommand, CommandError
-
-from django.contrib.auth.models import User
-
-# Why did they have to remove course_about api?!
-
 from xmodule.modulestore.django import modulestore
-from xmodule.modulestore.exceptions import ItemNotFoundError
-from student.models import CourseEnrollment, anonymous_id_for_user
-from openedx.core.lib.courses import course_image_url
 
-from lms.djangoapps.grades.new.course_grade_factory import CourseGradeFactory
+from kursitet.metadata import get_course_block
+
 
 class Command(BaseCommand):
     can_import_settings = True
@@ -31,65 +23,63 @@ class Command(BaseCommand):
     """
 
     option_list = BaseCommand.option_list + (
-        make_option('-m', '--meta_only',
-                    action='store_true',
-                    dest='meta_only',
-                    default=False,
-                    help='Do not collect grades, only output metadata.'),
-        make_option('-e', '--exclude',
-                    metavar='EXCLUDE_FILE',
-                    dest='exclude_file',
-                    default=False,
-                    help='Name of the list of excluded courses. Optional'),
-        make_option('-i', '--include',
-                    metavar='INCLUDE_FILE',
-                    dest='include_file',
-                    default=False,
-                    help='Name of the list of included courses. Optional'),
-        make_option('-c', '--course',
-                    metavar='SINGLE_COURSE',
-                    dest='single_course',
-                    default=False,
-                    help='Name of a single course to dump. Optional'),
-        make_option('-o', '--output',
-                    metavar='FILE',
-                    dest='output',
-                    default=False,
-                    help='Filename for grade output. JSON will be printed on stdout if this is missing.'))
+        make_option(
+            '-m',
+            '--meta_only',
+            action='store_true',
+            dest='meta_only',
+            default=False,
+            help='Do not collect grades, only output metadata.'), make_option(
+                '-e',
+                '--exclude',
+                metavar='EXCLUDE_FILE',
+                dest='exclude_file',
+                default=False,
+                help='Name of the list of excluded courses. Optional'),
+        make_option(
+            '-i',
+            '--include',
+            metavar='INCLUDE_FILE',
+            dest='include_file',
+            default=False,
+            help='Name of the list of included courses. Optional'),
+        make_option(
+            '-c',
+            '--course',
+            metavar='SINGLE_COURSE',
+            dest='single_course',
+            default=False,
+            help='Name of a single course to dump. Optional'), make_option(
+                '-o',
+                '--output',
+                metavar='FILE',
+                dest='output',
+                default=False,
+                help='Filename for grade output. '
+                'JSON will be printed on stdout if this is missing.'))
 
     def handle(self, *args, **options):
-
-        def get_detail(course_key, attribute):
-            usage_key = course_key.make_usage_key('about', attribute)
-            try:
-                value = modulestore().get_item(usage_key).data
-            except ItemNotFoundError:
-                value = None
-            return value
-
-        def iso_date(thing):
-            if isinstance(thing, datetime.datetime):
-                return thing.isoformat()
-            return thing
 
         exclusion_list = []
         inclusion_list = []
 
         if options['exclude_file']:
             try:
-                with open(options['exclude_file'],'rb') as exclusion_file:
+                with open(options['exclude_file'], 'rb') as exclusion_file:
                     data = exclusion_file.readlines()
                 exclusion_list = [x.strip() for x in data]
             except IOError:
-                raise CommandError("Could not read exclusion list from '{0}'".format(options['exclude_file']))
+                raise CommandError("Could not read exclusion list from '{0}'".
+                                   format(options['exclude_file']))
 
         if options['include_file']:
             try:
-                with open(options['include_file'],'rb') as inclusion_file:
+                with open(options['include_file'], 'rb') as inclusion_file:
                     data = inclusion_file.readlines()
                 inclusion_list = [x.strip() for x in data]
             except IOError:
-                raise CommandError("Could not read inclusion list from '{0}'".format(options['include_file']))
+                raise CommandError("Could not read inclusion list from '{0}'".
+                                   format(options['include_file']))
 
         store = modulestore()
         epoch = int(time.time())
@@ -106,7 +96,7 @@ class Command(BaseCommand):
                 if course_id_string not in [options['single_course'].strip()]:
                     continue
             elif inclusion_list:
-                if not course_id_string in inclusion_list:
+                if course_id_string not in inclusion_list:
                     continue
             elif exclusion_list:
                 if course_id_string in exclusion_list:
@@ -114,66 +104,17 @@ class Command(BaseCommand):
 
             print "Processing {}".format(course_id_string)
 
-            students = CourseEnrollment.objects.users_enrolled_in(course.id)
-
-            course_block = {
-              'id': course_id_string,
-              'meta_data': {
-                'about': {
-                    'display_name': course.display_name,
-                    'media': {
-                        'course_image': course_image_url(course),
-                    }
-                },
-                # Yes, I'm duplicating them for now, because the about section is shot.
-                'display_name': course.display_name,
-                'banner': course_image_url(course),
-                'id_org': course.org,
-                'id_number': course.number,
-                'graded': course.graded,
-                'hidden': course.visible_to_staff_only,
-                'ispublic': not (course.visible_to_staff_only or False), # course.ispublic was removed in dogwood.
-                'grading_policy': course.grading_policy,
-                'advanced_modules': course.advanced_modules,
-                'lowest_passing_grade': course.lowest_passing_grade,
-                'start': iso_date(course.start),
-                'advertised_start': iso_date(course.advertised_start),
-                'end': iso_date(course.end),
-                'enrollment_end': iso_date(course.enrollment_end),
-                'enrollment_start': iso_date(course.enrollment_start),
-                'has_started': course.has_started(),
-                'has_ended': course.has_ended(),
-                'overview': get_detail(course.id,'overview'),
-                'short_description': get_detail(course.id,'short_description'),
-                'pre_requisite_courses': get_detail(course.id,'pre_requisite_courses'),
-                'video': get_detail(course.id,'video'),
-              },
-              'students': [x.username for x in students],
-              'global_anonymous_id': { x.username:anonymous_id_for_user(x, None) for x in students },
-              'local_anonymous_id': { x.username:anonymous_id_for_user(x, course.id) for x in students },
-            }
-
+            course_block = get_course_block(
+                course, get_grades=not options['meta_only'])
             if not options['meta_only']:
                 blob['grading_data_epoch'] = epoch
-                course_block['grading_data'] = []
-                # Grab grades for all students that have ever had anything to do with the course.
-                graded_students = User.objects.filter(pk__in=CourseEnrollment.objects.filter(course_id=course.id).values_list('user',flat=True))
-                print "{0} graded students in course {1}".format(graded_students.count(),course_id_string)
-                if graded_students.count():
-                    for student in graded_students:
-                        grade = CourseGradeFactory().create(student, course)
-                        course_block['grading_data'].append({
-                            'username': student.username,
-                            'grades': {
-                                 'grade': grade.letter_grade,
-                                 'percent': grade.percent,
-                                 'summary': grade.summary,
-                            }
-                        })
             blob['courses'].append(course_block)
+
         if options['output']:
             # Ensure the dump is atomic.
-            with tempfile.NamedTemporaryFile('w', dir=os.path.dirname(options['output']), delete=False) as output_file:
+            with tempfile.NamedTemporaryFile(
+                    'w', dir=os.path.dirname(options['output']),
+                    delete=False) as output_file:
                 json.dump(blob, output_file)
                 tempname = output_file.name
             os.rename(tempname, options['output'])
---
+++ common/djangoapps/kursitet/metadata.py
@@ -0,0 +1,186 @@
+"""
+
+Common functionality used to extract the data kursitet wants.
+
+"""
+
+import datetime
+from django.contrib.auth.models import User
+from django.test.client import RequestFactory
+from student.models import CourseEnrollment, anonymous_id_for_user
+from lms.djangoapps.grades.new.course_grade_factory import CourseGradeFactory
+from openedx.core.lib.courses import course_image_url
+from xmodule.modulestore.django import modulestore
+from xmodule.modulestore.exceptions import ItemNotFoundError
+from course_api.blocks.api import get_blocks
+
+
+def get_student_grades(course):
+    """
+    Return all the student grades for a given course.
+    Expects a course object rather than a course locator.
+    """
+
+    data_block = []
+
+    graded_students = User.objects.filter(
+        pk__in=CourseEnrollment.objects.filter(
+            course_id=course.id).values_list('user', flat=True))
+    if graded_students.count():
+        for student in graded_students:
+            grade = CourseGradeFactory().create(student, course)
+            data_block.append({
+                'username': student.username,
+                'grades': {
+                    'grade': grade.letter_grade,
+                    'percent': grade.percent,
+                    'summary': grade.summary,
+                }
+            })
+    return data_block
+
+
+def get_course_block(course, get_grades=False):
+    """
+    Return a blob of course metadata the way kursitet likes it.
+    Expects a course object rather than a course locator.
+    """
+
+    def iso_date(thing):
+        if isinstance(thing, datetime.datetime):
+            return thing.isoformat()
+        return thing
+
+    def get_detail(course_key, attribute):
+        usage_key = course_key.make_usage_key('about', attribute)
+        try:
+            value = modulestore().get_item(usage_key).data
+        except ItemNotFoundError:
+            value = None
+        return value
+
+    course_id_string = course.id.to_deprecated_string()
+    store = modulestore()
+
+    students = CourseEnrollment.objects.users_enrolled_in(course.id)
+
+    # For course TOC we need a user and a request. Find the first superuser defined,
+    # that will be our user.
+    request_user = User.objects.filter(is_superuser=True).first()
+    factory = RequestFactory()
+
+    # The method of getting a table of contents for a course is quite obtuse.
+    # We have to go all the way to simulating a request.
+
+    request = factory.get('/')
+    request.user = request_user
+
+    raw_blocks = get_blocks(
+        request,
+        store.make_course_usage_key(course.id),
+        request_user,
+        requested_fields=[
+            'id', 'type', 'display_name', 'children', 'lms_web_url'
+        ])
+
+    # We got the block structure. Now we need to massage it so we get the
+    # proper jump urls without the site domain.
+    # Because on the test server the site domain is wrong.
+    blocks = {}
+    for block_key, block in raw_blocks['blocks'].items():
+        try:
+            direct_url = '/courses/' + \
+                block.get('lms_web_url').split('/courses/')[1]
+        except IndexError:
+            direct_url = ''
+        blocks[block_key] = {
+            'id': block.get('id', ''),
+            'display_name': block.get('display_name', ''),
+            'type': block.get('type', ''),
+            'children_ids': block.get('children', []),
+            'url': direct_url
+        }
+
+    # Then we need to recursively stitch it into a tree.
+
+    def _get_children(parent):
+        children = [
+            blocks.get(n) for n in parent['children_ids'] if blocks.get(n)
+        ]
+        for child in children:
+            child['children'] = _get_children(child)
+        parent['children'] = children
+        del parent['children_ids']
+        return children
+
+    block_tree = _get_children(blocks[raw_blocks['root']])
+
+    course_block = {
+        'id': course_id_string,
+        'meta_data': {
+            'about': {
+                'display_name': course.display_name,
+                'media': {
+                    'course_image': course_image_url(course),
+                }
+            },
+            'block_tree':
+            block_tree,
+            # Yes, I'm duplicating them for now, because the about section is shot.
+            'display_name':
+            course.display_name,
+            'banner':
+            course_image_url(course),
+            'id_org':
+            course.org,
+            'id_number':
+            course.number,
+            'graded':
+            course.graded,
+            'hidden':
+            course.visible_to_staff_only,
+            # course.ispublic was removed in dogwood.
+            'ispublic':
+            not (course.visible_to_staff_only or False),
+            'grading_policy':
+            course.grading_policy,
+            'advanced_modules':
+            course.advanced_modules,
+            'lowest_passing_grade':
+            course.lowest_passing_grade,
+            'start':
+            iso_date(course.start),
+            'advertised_start':
+            iso_date(course.advertised_start),
+            'end':
+            iso_date(course.end),
+            'enrollment_end':
+            iso_date(course.enrollment_end),
+            'enrollment_start':
+            iso_date(course.enrollment_start),
+            'has_started':
+            course.has_started(),
+            'has_ended':
+            course.has_ended(),
+            'overview':
+            get_detail(course.id, 'overview'),
+            'short_description':
+            get_detail(course.id, 'short_description'),
+            'pre_requisite_courses':
+            get_detail(course.id, 'pre_requisite_courses'),
+            'video':
+            get_detail(course.id, 'video'),
+        },
+        'students': [x.username for x in students],
+        'global_anonymous_id':
+        {x.username: anonymous_id_for_user(x, None)
+         for x in students},
+        'local_anonymous_id':
+        {x.username: anonymous_id_for_user(x, course.id)
+         for x in students},
+    }
+
+    if get_grades:
+        course_block['grading_data'] = get_student_grades(course)
+
+    return course_block
