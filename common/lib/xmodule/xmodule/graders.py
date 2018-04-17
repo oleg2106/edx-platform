60831f2771fef9f6fedb88c3c7b9135ceccd55b8
almipa <alex.pastut@gmail.com>
2018-04-17 17:40:34 +0300

    LevelFormatGrader for #14475

--- common/lib/xmodule/xmodule/graders.py
+++ common/lib/xmodule/xmodule/graders.py
@@ -152,6 +152,30 @@ def invalid_args(func, argdict):
         return set()  # All accepted
     return set(argdict) - set(args)
 
+import re
+level_pattern = re.compile(r'(.*)\((LG):(\d+)\)')
+
+def is_level_grader(name):
+    res = re.match(level_pattern, name)
+    if res is not None:
+       return res.group(2) == 'LG'
+    else:
+       return False
+
+def get_level_grader_value(name):
+    res = re.match(level_pattern, name)
+    if res is not None:
+       return int(res.group(3))
+    else:
+       return 0
+
+def get_level_grader_name(name):
+    res = re.match(level_pattern, name)
+    if res is not None:
+       return res.group(1)
+    else:
+       return name
+
 
 def grader_from_conf(conf):
     """
@@ -162,9 +186,6 @@ def grader_from_conf(conf):
     the parameters for making an AssignmentFormatGrader, in addition to a 'weight' key.
     """
 
-    # import pudb
-    # pudb.set_trace()
-
     if isinstance(conf, CourseGrader):
         return conf
 
@@ -174,7 +195,7 @@ def grader_from_conf(conf):
         weight = subgraderconf.pop("weight", 0)
         try:
             if 'min_count' in subgraderconf:
-                if subgraderconf['short_label'] == '->LG':
+                if is_level_grader(subgraderconf['short_label']):
                     #This is an LevelFormatGrader
                     subgrader_class = LevelFormatGrader
                 else:
@@ -274,8 +295,13 @@ class WeightedSubsectionsGrader(CourseGrader):
         total_percent = 0.0
         section_breakdown = []
         grade_breakdown = OrderedDict()
+        level_grade = False
 
         for subgrader, assignment_type, weight in self.subgraders:
+            
+            if isinstance(subgrader, LevelFormatGrader):
+               level_grade = True
+
             subgrade_result = subgrader.grade(grade_sheet, generate_random_scores)
 
             weighted_percent = subgrade_result['percent'] * weight
@@ -294,7 +320,8 @@ class WeightedSubsectionsGrader(CourseGrader):
         return {
             'percent': total_percent,
             'section_breakdown': section_breakdown,
-            'grade_breakdown': grade_breakdown
+            'grade_breakdown': grade_breakdown,
+            'level_grade': level_grade
         }
 
 
@@ -353,6 +380,9 @@ class AssignmentFormatGrader(CourseGrader):
         self.show_only_average = show_only_average
         self.starting_index = starting_index
         self.hide_average = hide_average
+    
+    def _calc_percentage(self, earned, possible):
+        return earned / possible
 
     def grade(self, grade_sheet, generate_random_scores=False):
         def total_with_drops(breakdown, drop_count):
@@ -390,7 +420,7 @@ class AssignmentFormatGrader(CourseGrader):
                     possible = scores[i].graded_total.possible
                     section_name = scores[i].display_name
 
-                percentage = earned / possible
+                percentage = self._calc_percentage(earned, possible)
                 summary_format = u"{section_type} {index} - {name} - {percent:.0%} ({earned:.3n}/{possible:.3n})"
                 summary = summary_format.format(
                     index=i + self.starting_index,
@@ -456,39 +486,9 @@ class AssignmentFormatGrader(CourseGrader):
         }
 
 
-class LevelFormatGrader(CourseGrader):
+class LevelFormatGrader(AssignmentFormatGrader):
     """
-    Grades all sections matching the format 'type' with an equal weight. A specified
-    number of lowest scores can be dropped from the calculation. The minimum number of
-    sections in this format must be specified (even if those sections haven't been
-    written yet).
-
-    min_count defines how many assignments are expected throughout the course. Placeholder
-    scores (of 0) will be inserted if the number of matching sections in the course is < min_count.
-    If there number of matching sections in the course is > min_count, min_count will be ignored.
-
-    show_only_average is to suppress the display of each assignment in this grader and instead
-    only show the total score of this grader in the breakdown.
-
-    hide_average is to suppress the display of the total score in this grader and instead
-    only show each assignment in this grader in the breakdown.
-
-    If there is only a single assignment in this grader, then it returns only one entry for the
-    grader.  Since the assignment and the total are the same, the total is returned but is not
-    labeled as an average.
-
-    category should be presentable to the user, but may not appear. When the grade breakdown is
-    displayed, scores from the same category will be similar (for example, by color).
-
-    section_type is a string that is the type of a singular section. For example, for Labs it
-    would be "Lab". This defaults to be the same as category.
-
-    short_label is similar to section_type, but shorter. For example, for Homework it would be
-    "HW".
-
-    starting_index is the first number that will appear. For example, starting_index=3 and
-    min_count = 2 would produce the labels "Assignment 3", "Assignment 4"
-
+    Special grader for one course Matvertical
     """
     def __init__(
             self,
@@ -502,119 +502,23 @@ class LevelFormatGrader(CourseGrader):
             hide_average=False,
             starting_index=1
     ):
-        self.type = type
-        self.min_count = min_count
-        self.drop_count = drop_count
-        self.category = category or self.type
-        self.section_type = section_type or self.type
-        self.short_label = short_label or self.type
-        self.show_only_average = show_only_average
-        self.starting_index = starting_index
-        self.hide_average = hide_average
+        super(LevelFormatGrader, self).__init__(
+            type,
+            min_count,
+            drop_count,
+            category,
+            section_type,
+            short_label,
+            show_only_average,
+            hide_average,
+            starting_index)
+        self.grader_level = get_level_grader_value(self.short_label)/100.0 
+        self.short_label = get_level_grader_name(self.short_label) 
 
-    def grade(self, grade_sheet, generate_random_scores=False):
-        def total_with_drops(breakdown, drop_count):
-            """
-            Calculates total score for a section while dropping lowest scores
-            """
-            # Create an array of tuples with (index, mark), sorted by mark['percent'] descending
-            sorted_breakdown = sorted(enumerate(breakdown), key=lambda x: -x[1]['percent'])
-
-            # A list of the indices of the dropped scores
-            dropped_indices = []
-            if drop_count > 0:
-                dropped_indices = [x[0] for x in sorted_breakdown[-drop_count:]]
-            aggregate_score = 0
-            for index, mark in enumerate(breakdown):
-                if index not in dropped_indices:
-                    aggregate_score += mark['percent']
-
-            if len(breakdown) - drop_count > 0:
-                aggregate_score /= len(breakdown) - drop_count
-
-            return aggregate_score, dropped_indices
-
-        scores = grade_sheet.get(self.type, {}).values()
-        breakdown = []
-        for i in range(max(self.min_count, len(scores))):
-            if i < len(scores) or generate_random_scores:
-                if generate_random_scores:  	# for debugging!
-                    earned = random.randint(2, 15)
-                    possible = random.randint(earned, 15)
-                    section_name = "Generated"
-
-                else:
-                    earned = scores[i].graded_total.earned
-                    possible = scores[i].graded_total.possible
-                    section_name = scores[i].display_name
-
-                percentage = earned / possible
-                percentage = 1 if percentage > 0.5 else 0 
-                summary_format = u"{section_type} {index} - {name} - {percent:.0%} ({earned:.3n}/{possible:.3n})"
-                summary = summary_format.format(
-                    index=i + self.starting_index,
-                    section_type=self.section_type,
-                    name=section_name,
-                    percent=percentage,
-                    earned=float(earned),
-                    possible=float(possible)
-                )
-            else:
-                percentage = 0.0
-                summary = u"{section_type} {index} Unreleased - 0% (?/?)".format(
-                    index=i + self.starting_index,
-                    section_type=self.section_type
-                )
-
-            short_label = u"{short_label} {index:02d}".format(
-                index=i + self.starting_index,
-                short_label=self.short_label
-            )
-
-            breakdown.append({'percent': percentage, 'label': short_label,
-                              'detail': summary, 'category': self.category})
-
-        total_percent, dropped_indices = total_with_drops(breakdown, self.drop_count)
-
-        for dropped_index in dropped_indices:
-            breakdown[dropped_index]['mark'] = {
-                'detail': u"The lowest {drop_count} {section_type} scores are dropped.".format(
-                    drop_count=self.drop_count,
-                    section_type=self.section_type
-                )
-            }
-
-        if len(breakdown) == 1:
-            # if there is only one entry in a section, suppress the existing individual entry and the average,
-            # and just display a single entry for the section.
-            total_detail = u"{section_type} = {percent:.0%}".format(
-                percent=total_percent,
-                section_type=self.section_type,
-            )
-            total_label = u"{short_label}".format(short_label=self.short_label)
-            breakdown = [{'percent': total_percent, 'label': total_label,
-                          'detail': total_detail, 'category': self.category, 'prominent': True}, ]
-        else:
-            total_detail = u"{section_type} Average = {percent:.0%}".format(
-                percent=total_percent,
-                section_type=self.section_type
-            )
-            total_label = u"{short_label} Avg".format(short_label=self.short_label)
-
-            if self.show_only_average:
-                breakdown = []
-
-            if not self.hide_average:
-                breakdown.append({'percent': total_percent, 'label': total_label,
-                                  'detail': total_detail, 'category': self.category, 'prominent': True})
-
-        return {
-            'percent': total_percent,
-            'section_breakdown': breakdown,
-            # No grade_breakdown here
-        }
-
-
+    def _calc_percentage(self, earned, possible):
+        percentage = earned / possible
+        return 1 if percentage >= self.grader_level else 0 
+        
 def _iter_graded(scores):
     """
     Yield the scores that belong to explicitly graded blocks
